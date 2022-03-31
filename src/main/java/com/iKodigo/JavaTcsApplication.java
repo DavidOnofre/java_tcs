0e8dcb6d8a17dc1d94a504ff2381e8c50d4c1ffc
DavidOnofre <88davido@gmail.com>
2022-03-31 15:45:12 -0500

    commit inicial test tcs donofre.

---
+++ src/main/java/com/iKodigo/JavaTcsApplication.java
@@ -0,0 +1,14 @@
+package com.iKodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTcsApplication.class, args);
+		System.out.println("iKodigo");
+	}
+
+}
